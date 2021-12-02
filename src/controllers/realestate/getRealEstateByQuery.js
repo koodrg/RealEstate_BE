f3c67b2d78609cc6d265319d310dbb764a953312
koodrg <56330018+koodrg@users.noreply.github.com>
2021-12-02 23:11:33 +0700

    search api

--- src/controllers/realestate/getRealEstateByQuery.js
+++ src/controllers/realestate/getRealEstateByQuery.js
@@ -1,86 +1,30 @@
-
+const { isValidObjectId } = require("mongoose");
 const { RealEstate } = require("../../models");
 
-const {response} = require('express');
-
-const getRealEstateByQuery = (req, res, next) => {
+const getRealEstateByQuery = async (req, res) => {
     let { q, category, price_max, price_min } = req.query;
-    console.log(req.query);
-  
-    console.log(q, category, price_max, price_min)
+    var response = [];
+    response = await RealEstate.find({
+        $and: [
+            {
+                $or: [
+                    { "full_address": { $regex: q || "" } },
+                    { "name": { $regex: q || "" }}
+                ]
+            },
+            {
+                $and: [
+                    { "price_by_num": { $lte: price_max || 100000000000 } },
+                    { "price_by_num": { $gte: price_min || 0} }
+                ]
+            },
+        ]
+    })
+    if(typeof q !== 'undefined'){
+        response = response.filter(x=>x.id_category == category)
+    }
+    res.status(200).send(response);
     
-    if(q==='null'){
-      //console.log('q null')
-      RealEstate.esSearch(
-        {
-          query: {
-            bool: {
-              must: [
-                {
-                  range: {
-                    price_by_num: {
-                      gte: price_min,
-                      lte: price_max,
-                    },
-                  },
-                },
-                { match: { id_category: category } },
-              ],
-            },
-          },
-        },
-        {
-          size: 60,
-          from: 0,
-        },
-        (err, result) => {
-          if (err) return next(err);
-          var data = result.hits.hits.map((hit) => {
-            hit._source._id = hit._id;
-            return hit;
-          });
-          response.data = data;
-          res.status(200).json(response);
-        }
-      );
-      return;
-    }
-    if(q.length > 0) {
-      //console.log('q not null')
-      RealEstate.esSearch(
-        {
-          query: {
-            bool: {
-              must: [
-                {
-                  range: {
-                    price_by_num: {
-                      gte: price_min,
-                      lte: price_max,
-                    },
-                  },
-                },
-                { match: { full_address: q}},
-                { match: { id_category: category } },
-              ],
-            },
-          },
-        },
-        {
-          size: 60,
-          from: 0,
-        },
-        (err, result) => {
-          if (err) return next(err);
-          var data = result.hits.hits.map((hit) => {
-            hit._source._id = hit._id;
-            return hit;
-          });
-          response.data = data;
-          res.status(200).json(response);
-        }
-      );
-    }
 }
 
 module.exports = getRealEstateByQuery;
